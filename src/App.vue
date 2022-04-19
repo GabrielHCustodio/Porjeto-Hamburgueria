105e474bc6e1e5eff0a0b58cca74d4bdcf8d19e1
GabrielHCustodio <93563385+GabrielHCustodio@users.noreply.github.com>
2022-04-18 23:27:03 -0300

    This is my commit by implementing Vue Router to the project, configuring information about the application pages, and adding the images.

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <NavbarHamb />
+  <router-view/>
+  <FooterHamb />
+</template>
+
+<script>
+import NavbarHamb from "./components/Navbar.vue"
+import FooterHamb from "./components/Footer.vue"
+
+export default {
+  components: {
+    NavbarHamb,
+    FooterHamb
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/pedidos">Pedidos</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NavbarHamb"
+}
+</script>
